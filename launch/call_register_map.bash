4b91afa884535f1b8eb10cac0553cfc4a018c08e
Jörg Stückler <joerg.stueckler.bw@gmail.com>
2014-07-11 13:20:02 +0200

    launch/bash files

---
+++ launch/call_register_map.bash
@@ -0,0 +1,3 @@
+#rosservice call /register_map/register '{ object_name: starter, init_pose: { position: { x: 0.0, y: 0.0, z: 0.0 }, orientation: { x: 0.0, y: 0.0, z: 0.0, w: 1.0 } } }'
+rosservice call /register_map/register '{ object_name: slam_starter, init_pose: { position: { x: -0.00796741, y: 0.0545351, z: 0.640077 }, orientation: { x: -0.03501, y: 0.993133, z: 0.110226, w: -0.0176319 } } }'
+
